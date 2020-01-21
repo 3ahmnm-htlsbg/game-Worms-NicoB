227b6205d90a491cd16483cf3315b34a846b66bf
NicoB <nicolas.bachleitner03@htl-salzburg.ac.at>
2020-01-21 15:41:44 +0100

    create player which can move with a and d and a launcher which can shoot with 2

---
+++ Assets/MyGame/Scripts/gunControll.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunControll : MonoBehaviour
+{
+    private Rigidbody rb;
+    public GameObject gunCubeSpawn;
+    private Vector3 gunCubeSpawnPos;
+    public GameObject gunCubeShoot;
+    private Vector3 gunCubeShootPos;
+    private Quaternion gunCubeSpawnRot;
+    public GameObject bullet;
+    private GameObject bulletInst;
+    private int i = 0;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("q"))
+        {
+            rb.AddTorque(0f, 0f, 5f);
+        }
+        if (Input.GetKey("e"))
+        {
+            rb.AddTorque(0f, 0f, -5f);
+        }
+        if (Input.GetKey("2"))
+        {
+            i--;
+            if (i < 0)
+            {
+                shoot();
+                i = 20;
+            }
+        }
+    }
+    void shoot()
+    {
+        Rigidbody rbBullet;
+        //get GunCube Pos and Rotation
+        gunCubeSpawnPos = gunCubeSpawn.transform.position;
+        gunCubeSpawnRot = gunCubeSpawn.transform.rotation;
+        //Instatiate
+        bulletInst = Instantiate(bullet, gunCubeSpawnPos, gunCubeSpawnRot);
+        //add force
+        gunCubeShootPos = gunCubeShoot.transform.position;
+        rbBullet = bulletInst.GetComponent<Rigidbody>();
+        rbBullet.AddForce(gunCubeShootPos, ForceMode.Impulse);
+    }
+}
---
+++ Assets/MyGame/Scripts/playerMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public bool jumpBool;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-.5f, 0f, 0f, ForceMode.Impulse);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(.5f, 0f, 0f, ForceMode.Impulse);
+        }
+        if (Input.GetKey("s") && jumpBool == true)
+        {
+            jump();
+            jumpBool = false;
+        }
+    }
+    void jump()
+    {
+        rb.AddForce(0f, 3f, 0f, ForceMode.Impulse);
+    }
+    void OnTriggerStay(Collider target)
+    {
+        if (target.tag == "cubeScene")
+        {
+            jumpBool = true;
+        }
+    }
+}
